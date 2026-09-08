ad036d0a840d0175f34948d7daf10804ca76761e
Paulo Dias <paulinndias05@gmail.com>
2026-09-08 14:11:38 -0300

    ci(): Workflow

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  defaultbrowser: 'chrome',
   reporter: 'cypress-multi-reporters',
   reporterOptions: {
     reporterEnabled: 'cypress-mochawesome-reporter, mocha--junit-reporter',
